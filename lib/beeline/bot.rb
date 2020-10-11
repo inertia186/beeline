c2f7789f616a120f73cd3d134984d1981b855394
Anthony Martin <github@martin-studio.com>
2020-10-11 14:53:34 -0700

    added cooldown

--- lib/beeline/bot.rb
+++ lib/beeline/bot.rb
@@ -20,22 +20,24 @@ module Beeline
     
     def process_chat_message(payload)
       from = payload['from']
+      
+      cooldown(from)
+      
+      conversation_id = payload['conversation_id']
       content = payload['content'].to_s
       command_key = content.split(' ').first.split(prefix).last.to_sym
       reply = if commands.keys.include? command_key
         args = (content.split(' ') - ["#{prefix}#{command_key}"]).join(' ')
         args = args.empty? ? nil : args
         
-        commands[command_key][:block].call(args, from)
+        commands[command_key][:block].call(args, from, conversation_id)
       elsif (matching_messages = messages.select{|k| Regexp.new(k).match?(content)}).any?
         message = matching_messages.values.first # match in order of declaration
         
-        message[:block].call(content, from)
+        message[:block].call(content, from, conversation_id)
       end
       
       if !!reply
-        conversation_id = payload['conversation_id']
-        
         chat_message(conversation_id, from, reply)
       end
     end
@@ -44,6 +46,28 @@ module Beeline
       return unless friendships[:accept] == 'auto'
       
       accept_pending_friend_requests
+    end
+  private
+    MIN_COOLDOWN = 0.25
+    BASE_COOLDOWN = 0.1
+    MAX_COOLDOWN = 90.0
+    
+    # Exponential backoff for key.  This will apply a rate-limit for each key
+    # that depends on how often the key is used.
+    # 
+    # @private
+    def cooldown(key)
+      @cooldown ||= {}
+      @cooldown[key] ||= Time.now
+      elapsed = Time.now - @cooldown[key]
+      
+      if elapsed > MAX_COOLDOWN
+        @cooldown[key] = nil
+      else
+        interval = [BASE_COOLDOWN * elapsed, MIN_COOLDOWN].max
+        
+        sleep interval
+      end
     end
   end
 end
